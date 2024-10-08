69815f7e83192a1e27ae37e4e194f5d3d209c85e
JustBottled <147569817+JustBottled@users.noreply.github.com>
2024-10-08 12:24:41 -0700

    Add files via upload

---
+++ MyInfo.py
@@ -0,0 +1,21 @@
+
+def printName():
+	print("Jianan Zhang")
+
+def printPronouns():
+	print("He, Him, His")
+
+def printFavFood():
+	print("My favorite food is braised pork rice. ")
+
+def printFavMovie():
+	print("My favorite movie are any of the John Wick movies.")
+
+def main():
+	printName()
+	printPronouns()
+	printFavFood()
+	printFavMovie()
+
+if __name__ == '__main__':
+	main()
