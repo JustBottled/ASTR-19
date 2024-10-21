da710dbdaa36b5a66b5483be08fb5b5c141517a2
JustBottled <147569817+JustBottled@users.noreply.github.com>
2024-10-21 10:42:45 -0700

    Utilized a longer method however using something new I learned.

--- Table.py
+++ Table.py
@@ -3,8 +3,9 @@ import numpy as np
 def table():
 
     x_elements = np.linspace(0, 2*np.pi, 1000)
+    sin_elements = np.sin(x_elements)
     for x in x_elements:
-        print(x, np.sin(x))
+        print(x_elements[x], sin_elements[x])
     
 def main():
     table()
